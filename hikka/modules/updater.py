002fa5dd6eeb7d33cd28b49c7180146dd263cc3e
NinoZOOM <107887743+NinoZOOM@users.noreply.github.com>
2022-06-22 14:31:51 +0500

    Update updater.py

--- hikka/modules/updater.py
+++ hikka/modules/updater.py
@@ -117,7 +117,7 @@ class UpdaterMod(loader.Module):
         self.config = loader.ModuleConfig(
             loader.ConfigValue(
                 "GIT_ORIGIN_URL",
-                "https://github.com/AmoreForever/Nino",
+                "https://github.com/Nino/Nino",
                 lambda: self.strings("origin_cfg_doc"),
                 validator=loader.validators.Link(),
             )

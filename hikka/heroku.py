4aa27a4eac2f935dfdb65c53c3a0440aa72c186e
NinoZOOM <107887743+NinoZOOM@users.noreply.github.com>
2022-06-22 14:05:27 +0500

    Update heroku.py

--- hikka/heroku.py
+++ hikka/heroku.py
@@ -102,7 +102,7 @@ def get_repo():
         repo = Repo.init(os.path.dirname(utils.get_base_dir()))
         origin = repo.create_remote(
             "origin",
-            "https://github.com/hikariatama/Hikka",
+            "https://github.com/NinoZOOM/Nino",
         )
         origin.fetch()
         repo.create_head("master", origin.refs.master)
